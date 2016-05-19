c8353ede9fb5e35315e8b76ce64b5d062f25b003
KerlyNaranjo <k.erly5@hotmail.com>
2016-05-19 07:34:08 -0500

    Menú de la prueba
    En cada opción agreguen la función correspondiente

---
+++ Ejercicio2.py
@@ -0,0 +1,33 @@
+#ngresar números hasta que el usuario presione 0 y 
+#retornar el promedio de los números
+
+#Menu
+#def menu():
+#	print()
+
+print ('\t\t\t\t\n        PRUEBA\n')
+print (' AUTORES: Kerly Naranjo')
+print ('          Katherine Lasluisa\n\t  Javier Chamba\n\t  Jazmin Villamarin')
+
+print ('\nEscoja el numero de ejercicio: \n')
+print ('1. Suma de vectores x, y, z')
+print ('2. Promedio de numeros')
+print ('3. Imvertir el contenido de un archivo')
+print ('4. Salir')
+
+opt = int(input("\nLa opcion es: "))
+
+if opt == 1:
+	#Agregar funcion
+	print(" ")
+
+elif opt == 2:
+	#Agregar funcion
+	print(" ")
+
+elif opt == 3:
+	#Agregar funcion
+	print(" ")
+
+elif opt == 4:
+	exit()
