37aed4dfbbd26c77e9932e27bfece21b4c1870fe
JazminVS <jazminv8.95@gmail.com>
2016-05-19 09:20:01 -0500

    VERSION SEMIFINAL

--- ejercicicio3.py
+++ ejercicicio3.py
@@ -1,8 +1,8 @@
 #import os
 archivo=open("Narvaez.txt","r")
 context=archivo.read()
-print("Numero de palabras: " + str(len(context.split(" "))))
-
+print("ARCHIVO ORIGINAL (#DE PALABRAS): " + str(len(context.split(" "))))
+print("-----------------------------------")
 pr1=context[0:56999]
 pr2=context[56999:113998]
 pr3=context[113999:]
@@ -11,24 +11,30 @@ p1=open("File1.txt","w")
 p1.write(pr1)
 p1=open("File1.txt","r")
 a=p1.read()
-a.reverse()
-print (str(a))
-print("Archivo1: " + str(len(a.split(" "))))
+print("Archivo1 (#DE PALABRAS): " + str(len(a.split(" "))))
+#print("La cadena invertida es:",a[::-1])
 p1.close()
+print("-----------------------------------")
 
 p2=open("File2.txt","w")
 p2.write(pr2)
 p2=open("File2.txt","r")
 b=p2.read()
-print("Archivo2: " + str(len(b.split(" "))))
+print("Archivo2 (#DE PALABRAS): " + str(len(b.split(" "))))
+
+
 p2.close()
+print("-----------------------------------")
 
 p3=open("File3.txt","w")
 p3.write(pr3)
 p3=open("File3.txt","r")
 c=p3.read()
-print("Archivo3: " + str(len(c.split(" "))))
+print("Archivo3 (#DE PALABRAS): " + str(len(c.split(" "))))
+i1=a[::-1]
+
 p3.close()
+print("-----------------------------------")
 
 
 
